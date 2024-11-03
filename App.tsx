bfc7b0ff96a5b9fee80fbcf110817e57f778cd23
Francis Padero <francispadero2001@gmail.com>
2024-11-03 22:50:00 +0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import WelcomeScreen from './app/screens/WelcomeScreen'
+
+const App = () => {
+  return (
+    <>
+    <WelcomeScreen/>
+    </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
